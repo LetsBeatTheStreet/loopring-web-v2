4b6c82c6bd4be50380375ce6c8eade1df360f582
Winda <windatang@gmail.com>
2021-12-15 20:55:18 +0800

    deposit_history_miss (#250)

--- packages/component-lib/src/components/modal/ModalPanels/HadAccount.tsx
+++ packages/component-lib/src/components/modal/ModalPanels/HadAccount.tsx
@@ -6,27 +6,20 @@ import { DepositRecorder } from './DepositRecorder';
 import { AccountHashInfo } from '@loopring-web/common-resources';
 
 export const HadAccount = withTranslation('common')(({mainBtn, t, ...props }: WithTranslation &
-  AccountBaseProps & {
-  clearDepositHash: () => void ,
-  // updateDepositHash: (depositHash: string,accountAddress:string,status?:'success'|'failed') => void,
-  chainInfos:AccountHashInfo}) => {
-  return <Box flex={1} display={'flex'} flexDirection={'column'} justifyContent={'space-between'} alignItems={'center'}>
-    <Box display={'flex'} flex={1}  justifyContent={'center'} alignItems={'center'}>
-      <AccountBasePanel {...props} t={t}/>
-    </Box>
-    {/*marginBottom={6}*/}
-    {/*<Box display={'flex'}  flexDirection={'column'} alignItems={'center'}>*/}
-    {/*    <Typography variant={'body2'} marginBottom={1} >*/}
-    {/*        {t('labelActivatedAccountDeposit')}*/}
-    {/*    </Typography>*/}
-    {/*</Box>*/}
-    <Box display={'flex'}  marginTop={2} alignSelf={'stretch'} paddingX={5} flexDirection={'column'} alignItems={'center'}>
-      {mainBtn}
-    </Box>
+    AccountBaseProps & {
+    clearDepositHash: () => void ,
+    chainInfos:AccountHashInfo}) => {
+    return <Box flex={1} display={'flex'} flexDirection={'column'} justifyContent={'space-between'} alignItems={'center'}>
+        <Box display={'flex'} flex={1}  justifyContent={'center'} alignItems={'center'}>
+            <AccountBasePanel {...props} t={t}/>
+        </Box>
+        <Box display={'flex'}  marginTop={2} alignSelf={'stretch'} paddingX={5} flexDirection={'column'} alignItems={'center'}>
+            {mainBtn}
+        </Box>
 
-    <Box display={'flex'} marginX={0}  marginTop={3} marginBottom={-5} alignSelf={'stretch'} paddingX={5} padding={0} >
-      <DepositRecorder  {...props} clear={props.clearDepositHash} t={t}/>
-    </Box>
+        <Box display={'flex'} marginX={0}  marginTop={3} marginBottom={-5} alignSelf={'stretch'} paddingX={5} padding={0} >
+            <DepositRecorder  {...props} clear={props.clearDepositHash} t={t}/>
+        </Box>
 
-  </Box>
+    </Box>
 })
